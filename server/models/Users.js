f1e3440b52aceb4fe0f6664582aeed66e128a2e3
“Tyler <tylermalone@me.com>
2023-03-23 13:54:14 -0400

    added user models

---
+++ server/models/Users.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const User = new mongoose.Schema({
+  userName: {
+    type: String,
+    max: 100,
+    required: true,
+    validate: /[a-z]/,
+    unique: true,
+    minlength: 1,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: /[a-z]/,
+    unique: true,
+    minlength: 1,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  practiceName: {
+    type: String,
+    required: false,
+  },
+  zipCode: {
+    type: Number,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("user", User);
