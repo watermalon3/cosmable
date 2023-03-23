04a9af02f06c2eb86953bcd0b46f7647b52b6116
JoseRodz33 <123189927+JoseRodz33@users.noreply.github.com>
2023-03-23 15:05:22 -0400

    Register Endpoint Completed

--- server/app.js
+++ server/app.js
@@ -6,8 +6,20 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 const HOST = process.env.HOST;
+const MONGO_URL = process.env.MONGO_URL
+
 
 app.use(cors());
+
+app.use(express.json());
+
+mongoose
+.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(console.log(`Connected to ${MONGO_URL}`))
+.catch(err => console.log(err))
 
 app.listen(PORT, HOST, () => {
   console.log(`[server] running on ${HOST}:${PORT}`);
