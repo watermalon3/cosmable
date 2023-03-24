a730d4b4cb5fb56ec2df142d64635dc7b63f3687
Michael Morris <denali5891@icloud.com>
2023-03-24 14:32:15 -0400

    update User and delete User

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -1,1 +1,41 @@
 const router = require("express").Router();
+const User = require('../models/User');
+
+
+router.put('/update/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const body = req.body;
+        const updatedUser = await User.updateOne({ _id: id }, { $set: body });
+
+        res.status(200).json({
+            message: `User successfully updated`,
+            updatedUser,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const findUser = await User.findbyIdAndDelete(id);
+        if (!findUser) {
+            throw new Error(`This user does not exist`);
+        } else {
+            res.status(200).json({
+                message: `User has been deleted`,
+            });
+        }
+    } catch (error) {
+        res.status(200).json({
+            message: error.message,
+        });
+    }
+});
+
+module.export = router;
+
