bf2b51f905f61bd49ec61f737a1b750641a16f65
Alexander Perry <thealexanderperry@outlook.com>
2023-03-28 14:17:14 -0400

    added custom URL endpoints for profiles, set to their name by default

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+
 import Bio from "./Components/Bio/Bio";
-// import Login from "./Components/login/login";
-// import Almost from "./Components/Almost/Almost";
+import Login from "./Components/login/Login";
+import Almost from "./Components/Almost/Almost";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      
+    <Login />
+    <Almost />
     <Bio />
       
     </>
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const SALT = Number(process.env.SALT);
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const slugify = require("slugify");
 
 router.post("/register", async (req, res) => {
   console.log(req.body)
@@ -15,6 +16,8 @@ router.post("/register", async (req, res) => {
       });
       throw new Error("The user has provided undefined schema values");
     }
+    const userSlug = slugify(name, { lower: true });
+    const profileUrl = `http://cosmable.com/profile/${userSlug}`;
     const newUser = new User({
       userName,
       email,
@@ -24,7 +27,8 @@ router.post("/register", async (req, res) => {
       practiceName,
       zipCode,
       occupation,
-      profilePicture
+      profilePicture,
+      profileUrl
     });
     await newUser.save();
 
@@ -64,11 +68,9 @@ router.post("/login", async (req, res) => {
           expiresIn: "24h",
         });
 
-        res.status(200).json({
-          message: "User Logged In",
-          foundUser,
-          token,
-        });
+        res.res.redirect(`/profile/${userSlug}`),
+        foundUser,
+        token
       } else {
         res.status(403).json({
           message: "Invalid Password",
