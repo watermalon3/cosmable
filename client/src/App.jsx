3fff2d4b9e45dd0f74dd71b36ec48a41e82fddf4
Michael Morris <denali5891@icloud.com>
2023-03-28 11:37:28 -0400

    Almost There and Bio in progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,35 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Login from "./Components/login/login";
-
+import Bio from "./Components/Bio/Bio";
+// import Login from "./Components/login/login";
+// import Almost from "./Components/Almost/Almost";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <Login />
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-    </div>
+    <>
+      
+    <Bio />
+      
+    </>
   );
 }
 
