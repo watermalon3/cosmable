8df3cc97279d9eba5cad773f8872e9c93cc04b28
Alexander Perry <thealexanderperry@outlook.com>
2023-03-29 11:03:48 -0400

    fixed mui errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,20 @@
 import "./App.css";
- feature-Nav
-import Login from "./Components/login/login";
 import ButtonAppBar from "./Components/Create/header/HeaderNav"
-import Almost from "./Components/almost/Almost";
 import HomePage from "./Components/home/HomePage";
 import LoginPage from "./Components/loginHome/HomeLogin";
-
-
-import Login from "./Components/loginHome/HomeLogin"
-
 import Bio from "./Components/Bio/Bio";
-
 import Almost from "./Components/Almost/Almost";
-function App() {
-  const [count, setCount] = useState(0);
-develop
 
 function App() {
   
   return (
     <>
- feature-Nav
       <ButtonAppBar/>
-      {/* <Login/> */}
       {/* <Almost/> */}
       {/* <HomePage/> */}
       <LoginPage/>
-
-    <Login />
-    <Almost />
-    <Bio />
+      <Bio />
       
-develop
     </>
   );
 }
