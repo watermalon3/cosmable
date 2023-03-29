9849a66692809a33796075a1da0381896dbead6c
Tyler Malone <tylermalone@me.com>
2023-03-29 15:44:36 -0400

    added portfoliolo fuctionality

---
+++ client/src/Components/UploadPorfolio/UploadPorfolio.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import AWS from "aws-sdk";
+import { Button } from "@mui/material";
+AWS.config.update({
+  accessKeyId: "AKIAWEVK55ISCRTPIXFB",
+  secretAccessKey: "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",
+  region: "us-east-1",
+  signatureVersion: "v4",
+});
+
+const ImageUploaderPortfolio = ({}) => {
+  const s3 = new AWS.S3();
+  const [imageUrl, setImageUrl] = useState(null);
+  const [file, setFile] = useState(null);
+
+  useEffect(() => {}, [imageUrl]);
+
+  const handleFileSelect = (e) => {
+    setFile(e.target.files[0]);
+  };
+  const uploadToS3 = async () => {
+    if (!file) {
+      return;
+    }
+    const params = {
+      Bucket: "cosmable",
+      Key: `${Date.now()}.${file.name}`,
+      Body: file,
+    };
+    const { Location } = await s3.upload(params).promise();
+    console.log("Location", Location);
+    setImageUrl(Location);
+
+    console.log("uploading to s3", Location);
+  };
+  return (
+    <div style={{ marginTop: "150px" }}>
+      <input type="file" onChange={handleFileSelect} />
+      {file && (
+        <div style={{ marginTop: "10px" }}>
+          <Button onClick={uploadToS3}>Upload</Button>
+        </div>
+      )}
+      {imageUrl && (
+        <div style={{ marginTop: "10px" }}>
+          <img src={imageUrl} alt="uploaded" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ImageUploaderPortfolio;
